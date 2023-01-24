6b52096d9807a93860003fe353c9a7d972d699b6
kuku1198 <kuku1198@gmail.com>
2023-01-24 11:43:03 +0900

    2-2-hello-api-ts

---
+++ 2-2-hello-api-ts/handler.ts
@@ -0,0 +1,13 @@
+import {APIGatewayProxyHandlerV2} from "aws-lambda"
+
+export const hello: APIGatewayProxyHandlerV2 = async (event) => {
+    console.log('event', event)
+    if (!event.queryStringParameters || !event.queryStringParameters.name) {
+        return {statusCode: 400, body: 'Not Found'}
+    }
+
+    return {
+        statusCode: 200,
+        body: `Hello, ${event.queryStringParameters.name}!`
+    }
+};
