5f420d695ecf8644034b012bc72c5b2ad66d9c32
kuku1198 <kuku1198@gmail.com>
2023-01-24 16:28:12 +0900

    2-4-hello-api-ts-with-webpack-source-map-support

---
+++ 2-4-hello-api-ts-with-webpack-source-map-support/handler.ts
@@ -0,0 +1,15 @@
+import {APIGatewayProxyHandlerV2} from "aws-lambda"
+import "source-map-support/register"
+
+export const hello: APIGatewayProxyHandlerV2 = async (event) => {
+    console.log('event', event)
+
+    if (!event.queryStringParameters || !event.queryStringParameters.name) {
+        return {statusCode: 400, body: 'Not Found'}
+    }
+
+    return {
+        statusCode: 200,
+        body: `Hello, ${event.queryStringParameters.name}!`
+    }
+};
